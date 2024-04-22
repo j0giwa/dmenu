bf446f3c0ae083eac33360623d017bb9669907a2
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-04-22 09:53:35 +0200

    Updated config

--- config.def.h
+++ config.def.h
@@ -3,19 +3,18 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const int user_bh = 0;       /* 0 means that dmenu will calculate bar height, >= 1 means dwm will use user_bh as bar height */
+static const int user_bh = 20;       /* 0 means that dmenu will calculate bar height, >= 1 means dwm will use user_bh as bar height */
 
-static const char *fonts[] = {
-	"monospace:size=10"
-};
+static const char *fonts[]          = {"Iosevka:style:medium:size=10" ,"JetBrainsMono Nerd Font Mono:style:medium:size=17" };
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#888888", "#222222" },
+	[SchemeSel] = { "#ffffff", "#285577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
